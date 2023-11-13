2d085392aa16091e01513571ce87d158d1dc6fe6
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-13 10:57:40 -0600

    Installed all dependencies and packages

---
+++ client/src/utils/queries.js
@@ -0,0 +1,40 @@
+import { gql } from "@apollo/client";
+
+// export const QUERY_PROFILES = gql`
+//   query allProfiles {
+//     profiles {
+//       _id
+//       name
+//       skills
+//     }
+//   }
+// `;
+
+// export const QUERY_SINGLE_PROFILE = gql`
+//   query singleProfile($profileId: ID!) {
+//     profile(profileId: $profileId) {
+//       _id
+//       name
+//       skills
+//     }
+//   }
+// `;
+
+export const QUERY_ME = gql`
+ {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+      }
+    }
+  }
+`;
