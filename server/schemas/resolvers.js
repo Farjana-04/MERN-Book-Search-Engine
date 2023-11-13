4671dd70f7657fe47b08251a353b3ed39f6cb66d
Farjana Akhter <farjana.akhter085@gmail.com>
2023-11-13 11:28:53 -0600

    added code in mutation for addUser,login, saveBook and removeBook in server/schemas/resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,12 +3,12 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    
+
     //By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
       if (context.user) {
-      const userData = await User.findOne({ _id: context.user._id }).select("-__v, -password");
-      return userData;
+        const userData = await User.findOne({ _id: context.user._id }).select("-__v, -password");
+        return userData;
       }
       throw new AuthenticationError("Please log in");
     },
@@ -43,7 +43,7 @@ const resolvers = {
       console.log(context.user);
       // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
       if (context.user) {
-      const updateUser = await User.findByIdAndUpdate(
+        const updateUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           {
             $addToSet: { savedBooks: args },
@@ -56,21 +56,15 @@ const resolvers = {
         return updateUser;
       }
       // If user attempts to execute this mutation and isn't logged in, throw an error
-      throw  AuthenticationError;
+      throw AuthenticationError;
     },
-    // Set up mutation so a logged in user can only remove their profile and no one else's
-    // removeBook: async (parent, args, context) => {
-    //   if (context.user) {
-    //     return User.findOneAndDelete({ _id: context.user._id });
-    //   }
-    //   throw AuthenticationError;
-    // },
+
     // Make it so a logged in user can only remove a skill from their own profile
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         return User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: bookId} } },
+          { $pull: { savedBooks: { bookId: bookId } } },
           { new: true }
         );
       }
